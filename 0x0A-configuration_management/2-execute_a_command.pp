d0ddbcd58a0426a127b96f18d833069de5e2c5d2
youssefboussaid <youssbsd@gmail.com>
2022-01-03 15:49:39 -1100

    task

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
-#Using Puppet, create a manifest that kills a process named killmenow.
-exec { 'killmenow':
-path => '/usr/bin',
+# kills a process named killmenow using Puppet
+exec {'killmenow':
+path    => '/usr/bin',
 command => 'pkill -f killmenow'
 }
